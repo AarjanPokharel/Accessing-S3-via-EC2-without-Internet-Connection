63f79a3de6152de4f8b1b9d3f837f85900cea0d5
AarjanPokharel <104973899+AarjanPokharel@users.noreply.github.com>
2024-11-09 09:56:16 +0545

    Network terraform code part push

--- terraform.tf
+++ terraform.tf
@@ -8,4 +8,57 @@ resource "aws_vpc" "main" {
     enable_dns_hostnames = true
     enable_dns_support = true
 
+    tags = merge(local.common_tags, tomap({"Name" = "${local.prefix}-vpc"}))
 }
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.subnet_cidr[0]
+  map_public_ip_on_launch = true
+  availability_zone = "${var.region}a"
+
+  tags = merge(local.common_tags, tomap({"Name" = "${local.prefix}-publicSubnet"}))
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.subnet_cidr[1]
+  availability_zone = "${var.region}a"
+
+  tags = merge(local.common_tags, tomap({"Name" = "${local.prefix}-privateSubnet"}))
+}
+
+resource "aws_internet_gateway" "igw_main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, tomap({"Name" = "${local.prefix}-IGW"}))
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, tomap({"Name" = "${local.prefix}-publicRoute"}))
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, tomap({"Name" = "${local.prefix}-privateRoute"}))
+}
+
+resource "aws_route_table_association" "public_association" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.public.id
+}
+
+resource "aws_route_table_association" "private_association" {
+  route_table_id = aws_route_table.private.id
+  subnet_id = aws_subnet.private.id
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.igw_main.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
